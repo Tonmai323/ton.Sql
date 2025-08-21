7914ac0a16c229067772020a4207d93c2283c98b
Tonmai323 <pirat0653027802ton@gmail.com>
2025-08-21 13:54:36 +0700

    Update Lap7-21-8.sql

--- Lap7-21-8.sql
+++ Lap7-21-8.sql
@@ -1,1 +1,20 @@
 //ไพรัตน์ หลวงจุมพล 67040233125//
+Select c.CategortNAme, p.ProductName, p.UnitPrice
+From Products as p, Categores as c
+where p.CatrgoryID = c.CategoryID
+and CatrgoryName = 'saefood'
+
+Select c.CategortName, p.ProductName, p.UnitPrice
+From Products as p join Categores as c
+on p.CategoryID = c.CategoryID
+where p.CatrgoryName = 'sefood'
+
+Select ComepanyName , OrderID
+From OrderID, Shipers
+where Shipers.ShipersID = OrderID.Shipvia
+and OrderID = 10275
+
+Select ComepanyName , OrderID
+From OrderID, Shipers
+where Shipers.ShipersID = OrderID.Shipvia
+and OrderID = 10275
